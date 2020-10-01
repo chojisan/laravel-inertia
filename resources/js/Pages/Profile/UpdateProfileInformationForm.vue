a337ce4ec30f435ca18a02b1e9b75917d15c4d98
Seigi Takada <choji.takada@gmail.com>
2020-10-01 22:07:31 +0800

    Update fields

--- resources/js/Pages/Profile/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/UpdateProfileInformationForm.vue
@@ -10,44 +10,92 @@
 
         <template #form>
             <!-- Profile Photo -->
-            <div class="col-span-6 sm:col-span-4" v-if="$page.jetstream.managesProfilePhotos">
+            <div
+                class="col-span-6 sm:col-span-4"
+                v-if="$page.jetstream.managesProfilePhotos"
+            >
                 <!-- Profile Photo File Input -->
-                <input type="file" class="hidden"
-                            ref="photo"
-                            @change="updatePhotoPreview">
+                <input
+                    type="file"
+                    class="hidden"
+                    ref="photo"
+                    @change="updatePhotoPreview"
+                />
 
                 <jet-label for="photo" value="Photo" />
 
                 <!-- Current Profile Photo -->
-                <div class="mt-2" v-show="! photoPreview">
-                    <img :src="$page.user.profile_photo_url" class="rounded-full h-20 w-20">
+                <div class="mt-2" v-show="!photoPreview">
+                    <img
+                        :src="$page.user.profile_photo_url"
+                        class="rounded-full h-20 w-20"
+                    />
                 </div>
 
                 <!-- New Profile Photo Preview -->
                 <div class="mt-2" v-show="photoPreview">
-                    <span class="block rounded-full w-20 h-20"
-                          :style="'background-size: cover; background-repeat: no-repeat; background-position: center center; background-image: url(\'' + photoPreview + '\');'">
+                    <span
+                        class="block rounded-full w-20 h-20"
+                        :style="
+                            'background-size: cover; background-repeat: no-repeat; background-position: center center; background-image: url(\'' +
+                                photoPreview +
+                                '\');'
+                        "
+                    >
                     </span>
                 </div>
 
-                <jet-secondary-button class="mt-2" type="button" @click.native.prevent="selectNewPhoto">
+                <jet-secondary-button
+                    class="mt-2"
+                    type="button"
+                    @click.native.prevent="selectNewPhoto"
+                >
                     Select A New Photo
                 </jet-secondary-button>
 
                 <jet-input-error :message="form.error('photo')" class="mt-2" />
             </div>
 
-            <!-- Name -->
+            <!-- First Name -->
             <div class="col-span-6 sm:col-span-4">
-                <jet-label for="name" value="Name" />
-                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" autocomplete="name" />
-                <jet-input-error :message="form.error('name')" class="mt-2" />
+                <jet-label for="first_name" value="First Name" />
+                <jet-input
+                    id="first_name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.first_name"
+                    autocomplete="first_name"
+                />
+                <jet-input-error
+                    :message="form.error('first_name')"
+                    class="mt-2"
+                />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+                <jet-label for="last_name" value="Last Name" />
+                <jet-input
+                    id="last_name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.last_name"
+                    autocomplete="last_name"
+                />
+                <jet-input-error
+                    :message="form.error('last_name')"
+                    class="mt-2"
+                />
             </div>
 
             <!-- Email -->
             <div class="col-span-6 sm:col-span-4">
                 <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" />
+                <jet-input
+                    id="email"
+                    type="email"
+                    class="mt-1 block w-full"
+                    v-model="form.email"
+                />
                 <jet-input-error :message="form.error('email')" class="mt-2" />
             </div>
         </template>
@@ -57,7 +105,10 @@
                 Saved.
             </jet-action-message>
 
-            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+            <jet-button
+                :class="{ 'opacity-25': form.processing }"
+                :disabled="form.processing"
+            >
                 Save
             </jet-button>
         </template>
@@ -65,67 +116,71 @@
 </template>
 
 <script>
-    import JetButton from './../../Jetstream/Button'
-    import JetFormSection from './../../Jetstream/FormSection'
-    import JetInput from './../../Jetstream/Input'
-    import JetInputError from './../../Jetstream/InputError'
-    import JetLabel from './../../Jetstream/Label'
-    import JetActionMessage from './../../Jetstream/ActionMessage'
-    import JetSecondaryButton from './../../Jetstream/SecondaryButton'
+import JetButton from "./../../Jetstream/Button";
+import JetFormSection from "./../../Jetstream/FormSection";
+import JetInput from "./../../Jetstream/Input";
+import JetInputError from "./../../Jetstream/InputError";
+import JetLabel from "./../../Jetstream/Label";
+import JetActionMessage from "./../../Jetstream/ActionMessage";
+import JetSecondaryButton from "./../../Jetstream/SecondaryButton";
 
-    export default {
-        components: {
-            JetActionMessage,
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-            JetSecondaryButton,
-        },
+export default {
+    components: {
+        JetActionMessage,
+        JetButton,
+        JetFormSection,
+        JetInput,
+        JetInputError,
+        JetLabel,
+        JetSecondaryButton
+    },
 
-        props: ['name', 'email'],
+    props: ["first_name", "last_name", "email"],
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    '_method': 'PUT',
-                    name: this.name,
+    data() {
+        return {
+            form: this.$inertia.form(
+                {
+                    _method: "PUT",
+                    first_name: this.first_name,
+                    last_name: this.last_name,
                     email: this.email,
-                    photo: null,
-                }, {
-                    bag: 'updateProfileInformation',
-                    resetOnSuccess: false,
-                }),
-
-                photoPreview: null,
-            }
-        },
-
-        methods: {
-            updateProfileInformation() {
-                if (this.$refs.photo) {
-                    this.form.photo = this.$refs.photo.files[0]
+                    photo: null
+                },
+                {
+                    bag: "updateProfileInformation",
+                    resetOnSuccess: false
                 }
+            ),
 
-                this.form.post('/user/profile-information', {
-                    preserveScroll: true
-                });
-            },
+            photoPreview: null
+        };
+    },
 
-            selectNewPhoto() {
-                this.$refs.photo.click();
-            },
-
-            updatePhotoPreview() {
-                const reader = new FileReader();
-
-                reader.onload = (e) => {
-                    this.photoPreview = e.target.result;
-                };
-
-                reader.readAsDataURL(this.$refs.photo.files[0]);
+    methods: {
+        updateProfileInformation() {
+            if (this.$refs.photo) {
+                this.form.photo = this.$refs.photo.files[0];
             }
+
+            this.form.post("/user/profile-information", {
+                preserveScroll: true
+            });
         },
+
+        selectNewPhoto() {
+            this.$refs.photo.click();
+        },
+
+        updatePhotoPreview() {
+            const reader = new FileReader();
+
+            reader.onload = e => {
+                this.photoPreview = e.target.result;
+            };
+
+            reader.readAsDataURL(this.$refs.photo.files[0]);
+        }
     }
+};
 </script>
