16150915bfa66dc6f7be40ea8b2f6ffaf25d08ff
Seigi Takada <choji.takada@gmail.com>
2020-10-08 22:04:01 +0800

    Fix padding of flash message

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="flex h-screen bg-gray-200">
-        <!-- Sidebar -->
-        <sidebar />
-        <div class="flex-1 flex flex-col overflow-hidden">
-            <!-- Header -->
-            <admin-header />
-            <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-                <div class="container mx-auto">
-                    <flash-messages />
+  <div class="flex h-screen bg-gray-200">
+    <!-- Sidebar -->
+    <sidebar />
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <!-- Header -->
+      <admin-header />
+      <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+        <div class="container mx-auto">
+          <flash-messages
+            class="w-full justify-between mx-auto py-6 px-4 sm:px-6 lg:px-8"
+          />
 
-                    <!-- Page Heading -->
-                    <header>
-                        <div
-                            class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"
-                        >
-                            <slot name="header"></slot>
-                        </div>
-                    </header>
+          <!-- Page Heading -->
+          <header>
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+              <slot name="header"></slot>
+            </div>
+          </header>
 
-                    <!-- Page Content -->
-                    <slot></slot>
+          <!-- Page Content -->
+          <slot></slot>
 
-                    <!-- Modal Portal -->
-                    <portal-target name="modal" multiple></portal-target>
-                </div>
-            </main>
+          <!-- Modal Portal -->
+          <portal-target name="modal" multiple></portal-target>
         </div>
+      </main>
     </div>
+  </div>
 </template>
 
 <script>
@@ -35,15 +35,15 @@ import AdminHeader from "./../Shared/AdminHeader";
 import Sidebar from "./../Shared/Sidebar";
 
 export default {
-    components: {
-        FlashMessages,
-        AdminHeader,
-        Sidebar
-    },
-    computed: {
-        path() {
-            return window.location.pathname;
-        }
+  components: {
+    FlashMessages,
+    AdminHeader,
+    Sidebar
+  },
+  computed: {
+    path() {
+      return window.location.pathname;
     }
+  }
 };
 </script>
