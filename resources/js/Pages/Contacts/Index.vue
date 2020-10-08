4fd48d9cf7f773d1e840f802ac4b5e3c3a576e95
Seigi Takada <choji.takada@gmail.com>
2020-10-08 15:29:25 +0800

    Update table template

--- resources/js/Pages/Contacts/Index.vue
+++ resources/js/Pages/Contacts/Index.vue
@@ -6,104 +6,149 @@
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="mb-6 flex justify-between items-center">
-                    <search-filter
-                        v-model="form.search"
-                        class="w-full max-w-md mr-4"
-                        @reset="reset"
+        <div class="my-6 px-4 sm:px-6 lg:px-8">
+            <div class="mt-3 flex justify-between items-center">
+                <search-filter
+                    v-model="form.search"
+                    class="w-full max-w-md mr-4"
+                    @reset="reset"
+                >
+                    <label class="block text-gray-700">Trashed:</label>
+                    <select
+                        v-model="form.trashed"
+                        class="mt-1 w-full form-select"
                     >
-                        <label class="block text-gray-700">Trashed:</label>
-                        <select
-                            v-model="form.trashed"
-                            class="mt-1 w-full form-select"
-                        >
-                            <option :value="null" />
-                            <option value="with">With Trashed</option>
-                            <option value="only">Only Trashed</option>
-                        </select>
-                    </search-filter>
-                    <link-button :link="route('contacts.create')">
-                        <span>Create Contact</span>
-                    </link-button>
-                </div>
-                <div class="bg-white rounded shadow overflow-x-auto">
-                    <table class="w-full whitespace-no-wrap">
-                        <tr class="text-left font-bold">
-                            <th class="px-6 pt-6 pb-4">Name</th>
-                            <th class="px-6 pt-6 pb-4">Organization</th>
-                            <th class="px-6 pt-6 pb-4">City</th>
-                            <th class="px-6 pt-6 pb-4" colspan="2">Phone</th>
-                        </tr>
-                        <tr
-                            v-for="contact in contacts.data"
-                            :key="contact.id"
-                            class="hover:bg-gray-100 focus-within:bg-gray-100"
-                        >
-                            <td class="border-t">
-                                <inertia-link
-                                    class="px-6 py-4 flex items-center focus:text-indigo-500"
-                                    :href="route('contacts.edit', contact.id)"
+                        <option :value="null" />
+                        <option value="with">With Trashed</option>
+                        <option value="only">Only Trashed</option>
+                    </select>
+                </search-filter>
+                <link-button :link="route('contacts.create')">
+                    <span>Create Contact</span>
+                </link-button>
+            </div>
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div
+                    class="inline-block min-w-full shadow rounded-lg overflow-hidden"
+                >
+                    <table class="min-w-full bg-white leading-normal">
+                        <thead>
+                            <tr class="text-left font-bold">
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
                                 >
-                                    {{ contact.name }}
-                                    <icon
-                                        v-if="contact.deleted_at"
-                                        name="trash"
-                                        class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2"
-                                    />
-                                </inertia-link>
-                            </td>
-                            <td class="border-t">
-                                <inertia-link
-                                    class="px-6 py-4 flex items-center"
-                                    :href="route('contacts.edit', contact.id)"
-                                    tabindex="-1"
+                                    Name
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
                                 >
-                                    <div v-if="contact.organization">
-                                        {{ contact.organization.name }}
-                                    </div>
-                                </inertia-link>
-                            </td>
-                            <td class="border-t">
-                                <inertia-link
-                                    class="px-6 py-4 flex items-center"
-                                    :href="route('contacts.edit', contact.id)"
-                                    tabindex="-1"
+                                    Organization
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
                                 >
-                                    {{ contact.city }}
-                                </inertia-link>
-                            </td>
-                            <td class="border-t">
-                                <inertia-link
-                                    class="px-6 py-4 flex items-center"
-                                    :href="route('contacts.edit', contact.id)"
-                                    tabindex="-1"
+                                    City
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                                    colspan="2"
                                 >
-                                    {{ contact.phone }}
-                                </inertia-link>
-                            </td>
-                            <td class="border-t w-px">
-                                <inertia-link
-                                    class="px-4 flex items-center"
-                                    :href="route('contacts.edit', contact.id)"
-                                    tabindex="-1"
+                                    Phone
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                v-for="contact in contacts.data"
+                                :key="contact.id"
+                                class="hover:bg-gray-100 focus-within:bg-gray-100"
+                            >
+                                <td class="border-t">
+                                    <inertia-link
+                                        class="px-5 py-5"
+                                        :href="
+                                            route('contacts.edit', contact.id)
+                                        "
+                                    >
+                                        {{ contact.name }}
+                                        <icon
+                                            v-if="contact.deleted_at"
+                                            name="trash"
+                                            class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2"
+                                        />
+                                    </inertia-link>
+                                </td>
+                                <td class="border-b border-gray-200 text-sm">
+                                    <inertia-link
+                                        class="px-5 py-5"
+                                        :href="
+                                            route('contacts.edit', contact.id)
+                                        "
+                                        tabindex="-1"
+                                    >
+                                        <div v-if="contact.organization">
+                                            {{ contact.organization.name }}
+                                        </div>
+                                    </inertia-link>
+                                </td>
+                                <td class="border-b border-gray-200 text-sm">
+                                    <inertia-link
+                                        class="px-5 py-5"
+                                        :href="
+                                            route('contacts.edit', contact.id)
+                                        "
+                                        tabindex="-1"
+                                    >
+                                        {{ contact.city }}
+                                    </inertia-link>
+                                </td>
+                                <td class="border-b border-gray-200 text-sm">
+                                    <inertia-link
+                                        class="px-5 py-5"
+                                        :href="
+                                            route('contacts.edit', contact.id)
+                                        "
+                                        tabindex="-1"
+                                    >
+                                        {{ contact.phone }}
+                                    </inertia-link>
+                                </td>
+                                <td
+                                    class="border-b border-gray-200 text-sm w-px"
                                 >
-                                    <icon
-                                        name="cheveron-right"
-                                        class="block w-6 h-6 fill-gray-400"
-                                    />
-                                </inertia-link>
-                            </td>
-                        </tr>
-                        <tr v-if="contacts.data.length === 0">
-                            <td class="border-t px-6 py-4" colspan="4">
-                                No contacts found.
-                            </td>
-                        </tr>
+                                    <inertia-link
+                                        class="px-5 py-5"
+                                        :href="
+                                            route('contacts.edit', contact.id)
+                                        "
+                                        tabindex="-1"
+                                    >
+                                        <icon
+                                            name="cheveron-right"
+                                            class="block w-6 h-6 fill-gray-400"
+                                        />
+                                    </inertia-link>
+                                </td>
+                            </tr>
+                            <tr v-if="contacts.data.length === 0">
+                                <td
+                                    class="px-5 py-5 border-b border-gray-200 text-sm"
+                                    colspan="4"
+                                >
+                                    No contacts found.
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
+
+                    <div
+                        class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
+                    >
+                        <div class="inline-flex mt-2 xs:mt-0">
+                            <pagination :links="contacts.links" />
+                        </div>
+                    </div>
                 </div>
-                <pagination :links="contacts.links" />
             </div>
         </div>
     </app-layout>
@@ -113,7 +158,7 @@
 import Icon from "./../../Shared/Icon";
 import AppLayout from "./../../Layouts/AppLayout";
 import mapValues from "lodash/mapValues";
-import Pagination from "./../../Shared/Pagination";
+import Pagination from "@/Shared/NewPagination";
 import pickBy from "lodash/pickBy";
 import SearchFilter from "./../../Shared/SearchFilter";
 import LinkButton from "./../../Shared/LinkButton";
--- resources/js/Pages/Organizations/Index.vue
+++ resources/js/Pages/Organizations/Index.vue
@@ -6,47 +6,62 @@
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="mb-6 flex justify-between items-center">
-                    <search-filter
-                        v-model="form.search"
-                        class="w-full max-w-md mr-4"
-                        @reset="reset"
+        <div class="my-6 px-4 sm:px-6 lg:px-8">
+            <div class="mt-3 flex justify-between items-center">
+                <search-filter
+                    v-model="form.search"
+                    class="w-full max-w-md mr-4"
+                    @reset="reset"
+                >
+                    <label class="block text-gray-700">Trashed:</label>
+                    <select
+                        v-model="form.trashed"
+                        class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500"
                     >
-                        <label class="block text-gray-700">Trashed:</label>
-                        <select
-                            v-model="form.trashed"
-                            class="mt-1 w-full form-select"
-                        >
-                            <option :value="null" />
-                            <option value="with">With Trashed</option>
-                            <option value="only">Only Trashed</option>
-                        </select>
-                    </search-filter>
-                    <link-button :link="route('organizations.create')">
-                        <span>Create Organization</span>
-                    </link-button>
-                </div>
+                        <option :value="null" />
+                        <option value="with">With Trashed</option>
+                        <option value="only">Only Trashed</option>
+                    </select>
+                </search-filter>
+                <link-button :link="route('organizations.create')">
+                    <span>Create Organization</span>
+                </link-button>
+            </div>
 
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="bg-white rounded shadow overflow-x-auto">
-                        <table class="w-full whitespace-no-wrap">
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div
+                    class="inline-block min-w-full shadow rounded-lg overflow-hidden"
+                >
+                    <table class="min-w-full bg-white leading-normal">
+                        <thead>
                             <tr class="text-left font-bold">
-                                <th class="px-6 pt-6 pb-4">Name</th>
-                                <th class="px-6 pt-6 pb-4">City</th>
-                                <th class="px-6 pt-6 pb-4" colspan="2">
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                                >
+                                    Name
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                                >
+                                    City
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                                    colspan="2"
+                                >
                                     Phone
                                 </th>
                             </tr>
+                        </thead>
+                        <tbody>
                             <tr
                                 v-for="organization in organizations.data"
                                 :key="organization.id"
                                 class="hover:bg-gray-100 focus-within:bg-gray-100"
                             >
-                                <td class="border-t">
+                                <td class="border-b border-gray-200 text-sm">
                                     <inertia-link
-                                        class="px-6 py-4 flex items-center focus:text-indigo-500"
+                                        class="px-5 py-5"
                                         :href="
                                             route(
                                                 'organizations.edit',
@@ -62,9 +77,9 @@
                                         />
                                     </inertia-link>
                                 </td>
-                                <td class="border-t">
+                                <td class="border-b border-gray-200 text-sm">
                                     <inertia-link
-                                        class="px-6 py-4 flex items-center"
+                                        class="px-5 py-5"
                                         :href="
                                             route(
                                                 'organizations.edit',
@@ -76,9 +91,9 @@
                                         {{ organization.city }}
                                     </inertia-link>
                                 </td>
-                                <td class="border-t">
+                                <td class="border-b border-gray-200 text-sm">
                                     <inertia-link
-                                        class="px-6 py-4 flex items-center"
+                                        class="px-5 py-5"
                                         :href="
                                             route(
                                                 'organizations.edit',
@@ -90,9 +105,11 @@
                                         {{ organization.phone }}
                                     </inertia-link>
                                 </td>
-                                <td class="border-t w-px">
+                                <td
+                                    class="border-b border-gray-200 text-sm w-px"
+                                >
                                     <inertia-link
-                                        class="px-4 flex items-center"
+                                        class="px-5 py-5"
                                         :href="
                                             route(
                                                 'organizations.edit',
@@ -109,14 +126,24 @@
                                 </td>
                             </tr>
                             <tr v-if="organizations.data.length === 0">
-                                <td class="border-t px-6 py-4" colspan="4">
+                                <td
+                                    class="px-5 py-5 border-b border-gray-200 text-sm"
+                                    colspan="4"
+                                >
                                     No organizations found.
                                 </td>
                             </tr>
-                        </table>
+                        </tbody>
+                    </table>
+
+                    <div
+                        class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
+                    >
+                        <div class="inline-flex mt-2 xs:mt-0">
+                            <pagination :links="organizations.links" />
+                        </div>
                     </div>
                 </div>
-                <pagination :links="organizations.links" />
             </div>
         </div>
     </app-layout>
@@ -126,7 +153,7 @@
 import Icon from "./../../Shared/Icon";
 import AppLayout from "./../../Layouts/AppLayout";
 import mapValues from "lodash/mapValues";
-import Pagination from "./../../Shared/Pagination";
+import Pagination from "./../../Shared/NewPagination";
 import pickBy from "lodash/pickBy";
 import SearchFilter from "./../../Shared/SearchFilter";
 import LinkButton from "./../../Shared/LinkButton";
