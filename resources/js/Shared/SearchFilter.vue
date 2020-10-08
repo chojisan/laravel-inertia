fa51e7231db9c9d2bb7310be268b8d4b8527ed4f
Seigi Takada <choji.takada@gmail.com>
2020-10-08 22:01:15 +0800

    Remove dropdown

--- resources/js/Shared/SearchFilter.vue
+++ resources/js/Shared/SearchFilter.vue
@@ -1,64 +1,45 @@
 <template>
-    <div class="flex items-center">
-        <div class="flex w-full bg-white shadow rounded">
-            <jet-dropdown
-                :auto-close="false"
-                class="px-4 md:px-6 rounded-l border-r hover:bg-gray-100 focus:border-white focus:shadow-outline focus:z-10"
-                placement="bottom-start"
-            >
-                <div class="flex items-baseline">
-                    <span class="text-gray-700 hidden md:inline">Filter</span>
-                    <svg
-                        class="w-2 h-2 fill-gray-700 md:ml-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 961.243 599.998"
-                    >
-                        <path
-                            d="M239.998 239.999L0 0h961.243L721.246 240c-131.999 132-240.28 240-240.624 239.999-.345-.001-108.625-108.001-240.624-240z"
-                        />
-                    </svg>
-                </div>
-                <div
-                    slot="dropdown"
-                    class="mt-2 px-4 py-6 w-screen shadow-xl bg-white rounded"
-                    :style="{ maxWidth: `${maxWidth}px` }"
-                >
-                    <slot />
-                </div>
-            </jet-dropdown>
-            <input
-                class="relative w-full px-6 py-3 rounded-r focus:shadow-outline"
-                autocomplete="off"
-                type="text"
-                name="search"
-                placeholder="Search…"
-                :value="value"
-                @input="$emit('input', $event.target.value)"
-            />
-        </div>
-        <button
-            class="ml-3 text-sm text-gray-500 hover:text-gray-700 focus:text-indigo-500"
-            type="button"
-            @click="$emit('reset')"
-        >
-            Reset
-        </button>
+  <div class="flex">
+    <div class="flex w-full bg-white shadow rounded">
+      <slot />
+      <div class="block relative mt-2 sm:mt-0">
+        <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+          <icon name="search" class="h-4 w-4 text-gray-500" />
+        </span>
+        <input
+          class="appearance-none rounded-r border-l border-gray-400 sm:rounded-l-none block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+          autocomplete="off"
+          type="text"
+          name="search"
+          placeholder="Search…"
+          :value="value"
+          @input="$emit('input', $event.target.value)"
+        />
+      </div>
     </div>
+    <button
+      class="ml-3 text-sm text-gray-500 hover:text-gray-700 focus:text-indigo-500"
+      type="button"
+      @click="$emit('reset')"
+    >
+      Reset
+    </button>
+  </div>
 </template>
 
 <script>
-import JetDropdown from "./../Jetstream/Dropdown";
+import Icon from "@/Shared/Icon";
 
 export default {
-    components: {
-        JetDropdown
-    },
-    props: {
-        value: String,
-        maxWidth: {
-            type: Number,
-            default: 300
-        }
+  props: {
+    value: String,
+    maxWidth: {
+      type: Number,
+      default: 300
     }
+  },
+  components: {
+    Icon
+  }
 };
 </script>
