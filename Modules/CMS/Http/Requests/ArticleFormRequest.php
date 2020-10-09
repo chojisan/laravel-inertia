0843572fc0ee0e07bd9147ca572a77de32e1d083
Seigi Takada <choji.takada@gmail.com>
2020-10-09 22:44:10 +0800

    Add article request form

---
+++ Modules/CMS/Http/Requests/ArticleFormRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\CMS\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'featured' => 'nullable',
+            'short_description' => 'nullable',
+            'description' => 'nullable',
+            'category_id' => 'required',
+            'tags' => 'nullable',
+            'image' => 'nullable',
+            'meta_description' => 'nullable',
+            'meta_keywords' => 'nullable',
+            'status' => 'required',
+        ];
+    }
+}
