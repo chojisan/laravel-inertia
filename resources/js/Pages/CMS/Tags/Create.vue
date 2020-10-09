400dc4210823d3fea6ae2d535587aef45f7c4896
Seigi Takada <choji.takada@gmail.com>
2020-10-09 22:47:16 +0800

    Add create tag page component

---
+++ resources/js/Pages/CMS/Tags/Create.vue
@@ -0,0 +1,58 @@
+<template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <inertia-link
+          class="text-indigo-400 hover:text-indigo-600"
+          :href="route('cms.tags.index')"
+          >Tags</inertia-link
+        >
+        <span class="text-indigo-400 font-medium">/</span> Create
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <tag-form
+          title="Create Tag"
+          description="Create tag description."
+          :form="form"
+          @submit="submit"
+        />
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import TagForm from "./TagForm";
+
+export default {
+  metaInfo: { title: "Create Tag" },
+  components: {
+    AppLayout,
+    TagForm
+  },
+  props: {
+    errors: Object
+  },
+  // remember: "form",
+  data() {
+    return {
+      sending: false,
+      form: this.$inertia.form({
+        name: null
+      })
+    };
+  },
+  methods: {
+    submit(form) {
+      this.sending = true;
+      this.$inertia
+        .post(this.route("cms.tags.store"), form)
+        .then(() => (this.sending = false));
+    }
+  }
+};
+</script>
