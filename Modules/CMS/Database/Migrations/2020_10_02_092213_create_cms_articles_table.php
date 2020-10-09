1c842cec6ee5335ab9a251590992cbf421e3a9c6
Seigi Takada <choji.takada@gmail.com>
2020-10-09 16:55:43 +0800

    Add cms articles table migration

---
+++ Modules/CMS/Database/Migrations/2020_10_02_092213_create_cms_articles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCmsArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cms_articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->boolean('featured')->default(0);
+            $table->string('image')->nullable();
+            $table->text('short_description')->nullable();
+            $table->text('description')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->integer('hits')->default(0);
+            $table->integer('order')->nullable();
+            $table->enum('status', ['PUBLISHED','DRAFT','PENDING']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cms_articles');
+    }
+}
