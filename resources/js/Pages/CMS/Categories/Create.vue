7a1ad1bdd37efc9e78e07986f57f58735a92c0fa
Seigi Takada <choji.takada@gmail.com>
2020-10-09 23:18:11 +0800

    Add create category page component

---
+++ resources/js/Pages/CMS/Categories/Create.vue
@@ -0,0 +1,64 @@
+<template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <inertia-link
+          class="text-indigo-400 hover:text-indigo-600"
+          :href="route('cms.categories.index')"
+          >Categories</inertia-link
+        >
+        <span class="text-indigo-400 font-medium">/</span> Create
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <category-form
+          title="Create Category"
+          description="Create category description."
+          :form="form"
+          @submit="submit"
+          :categories="categories"
+        />
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import CategoryForm from "./CategoryForm";
+
+export default {
+  metaInfo: { title: "Create Category" },
+  components: {
+    AppLayout,
+    CategoryForm
+  },
+  props: {
+    errors: Object,
+    categories: Array
+  },
+  // remember: "form",
+  data() {
+    return {
+      sending: false,
+      form: this.$inertia.form({
+        name: null,
+        description: null,
+        parent_id: null,
+        image: null,
+        published: 0
+      })
+    };
+  },
+  methods: {
+    submit(form) {
+      this.sending = true;
+      this.$inertia
+        .post(this.route("cms.categories.store"), form)
+        .then(() => (this.sending = false));
+    }
+  }
+};
+</script>
