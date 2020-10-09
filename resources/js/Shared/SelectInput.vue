53ef040006dcc2029326df1c23cde188f6cf1b79
Seigi Takada <choji.takada@gmail.com>
2020-10-09 13:43:48 +0800

    Fix format

--- resources/js/Shared/SelectInput.vue
+++ resources/js/Shared/SelectInput.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <label v-if="label" class="form-label" :for="id">{{ label }}:</label>
-    <select :id="id" ref="input" v-model="selected" v-bind="$attrs" class="form-select" :class="{ error: error }">
+    <select
+      :id="id"
+      ref="input"
+      v-model="selected"
+      v-bind="$attrs"
+      class="form-select"
+      :class="{ error: error }"
+    >
       <slot />
     </select>
     <div v-if="error" class="form-error">{{ error }}</div>
@@ -15,30 +22,30 @@ export default {
     id: {
       type: String,
       default() {
-        return `select-input-${this._uid}`
-      },
+        return `select-input-${this._uid}`;
+      }
     },
     value: [String, Number, Boolean],
     label: String,
-    error: String,
+    error: String
   },
   data() {
     return {
-      selected: this.value,
-    }
+      selected: this.value
+    };
   },
   watch: {
     selected(selected) {
-      this.$emit('input', selected)
-    },
+      this.$emit("input", selected);
+    }
   },
   methods: {
     focus() {
-      this.$refs.input.focus()
+      this.$refs.input.focus();
     },
     select() {
-      this.$refs.input.select()
-    },
-  },
-}
+      this.$refs.input.select();
+    }
+  }
+};
 </script>
