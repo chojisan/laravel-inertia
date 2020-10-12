0b7a4048a951c55660524fc2551230c5a4c0e20a
Seigi Takada <choji.takada@gmail.com>
2020-10-12 17:37:57 +0800

    Add site layout

---
+++ resources/js/Layouts/SiteLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <navbar-component></navbar-component>
+    <!-- Page Content -->
+    <slot></slot>
+
+    <!-- Modal Portal -->
+    <portal-target name="modal" multiple></portal-target>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/Shared/Navbar.vue";
+import FooterComponent from "@/Shared/Footer.vue";
+
+export default {
+  components: {
+    NavbarComponent,
+    FooterComponent
+  }
+};
+</script>
+
+<style></style>
