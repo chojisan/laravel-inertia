d95d74c3a06e701917a42c0be369f72798076651
Seigi Takada <choji.takada@gmail.com>
2020-10-12 18:33:18 +0800

    Add comments

--- Modules/CRM/Entities/Account.php
+++ Modules/CRM/Entities/Account.php
@@ -9,18 +9,39 @@ use Modules\CRM\Entities\Contact;
 
 class Account extends Model
 {
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'crm_accounts';
 
+    /**
+     * An account has many users.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function users()
     {
         return $this->hasMany(User::class);
     }
 
+    /**
+     * An account has many organizations.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function organizations()
     {
         return $this->hasMany(Organization::class);
     }
 
+     /**
+     * An account has many contacts.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function contacts()
     {
         return $this->hasMany(Contact::class);
--- Modules/CRM/Entities/Model.php
+++ Modules/CRM/Entities/Model.php
@@ -7,10 +7,27 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 abstract class Model extends Eloquent
 {
+    /**
+     * Don't auto-apply mass assignment protection.
+     *
+     * @var array
+     */
     protected $guarded = [];
 
+    /**
+     * Set view per page value.
+     *
+     * @var integer
+     */
     protected $perPage = 10;
 
+    /**
+     * Fix route model binding with trashed values.
+     *
+     * @param [type] $value
+     * @param [type] $field
+     * @return void
+     */
     public function resolveRouteBinding($value, $field = null)
     {
         return in_array(SoftDeletes::class, class_uses($this))
