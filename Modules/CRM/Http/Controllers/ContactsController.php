3c27f74ecffb672852908bb1782159eb7e687cc2
Seigi Takada <choji.takada@gmail.com>
2020-10-12 14:44:37 +0800

    Add table prefix crm_

--- Modules/CRM/Http/Controllers/ContactsController.php
+++ Modules/CRM/Http/Controllers/ContactsController.php
@@ -55,7 +55,7 @@ class ContactsController extends Controller
             Request::validate([
                 'first_name' => ['required', 'max:50'],
                 'last_name' => ['required', 'max:50'],
-                'organization_id' => ['nullable', Rule::exists('organizations', 'id')->where(function ($query) {
+                'organization_id' => ['nullable', Rule::exists('crm_organizations', 'id')->where(function ($query) {
                     $query->where('account_id', Auth::user()->account_id);
                 })],
                 'email' => ['nullable', 'max:50', 'email'],
@@ -102,7 +102,7 @@ class ContactsController extends Controller
             Request::validate([
                 'first_name' => ['required', 'max:50'],
                 'last_name' => ['required', 'max:50'],
-                'organization_id' => ['nullable', Rule::exists('organizations', 'id')->where(function ($query) {
+                'organization_id' => ['nullable', Rule::exists('crm_organizations', 'id')->where(function ($query) {
                     $query->where('account_id', Auth::user()->account_id);
                 })],
                 'email' => ['nullable', 'max:50', 'email'],
