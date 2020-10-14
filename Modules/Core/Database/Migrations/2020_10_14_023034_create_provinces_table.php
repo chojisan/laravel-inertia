d9d7c10b3b9a0ba022dcdac7490517c3f905430f
Seigi Takada <choji.takada@gmail.com>
2020-10-14 15:43:09 +0800

    Add province migration

---
+++ Modules/Core/Database/Migrations/2020_10_14_023034_create_provinces_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvincesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->foreignId('region_id');
+            $table->string('name');
+            $table->string('income_class');
+            $table->bigInteger('population');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provinces');
+    }
+}
