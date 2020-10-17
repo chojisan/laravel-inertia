4f62e994ecaf3b8812105faf7e764db3b247ed41
Seigi Takada <choji.takada@gmail.com>
2020-10-17 17:26:57 +0800

    Add shop order statuses migration

---
+++ Modules/Shop/Database/Migrations/2020_10_17_030925_create_shop_order_statuses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopOrderStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_order_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('is_default')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_order_statuses');
+    }
+}
