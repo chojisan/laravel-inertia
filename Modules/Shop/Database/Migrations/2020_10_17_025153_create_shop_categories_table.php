c214c386ccd506173729e63c0499c5b049525098
Seigi Takada <choji.takada@gmail.com>
2020-10-17 17:25:40 +0800

    Add shop categories migration

---
+++ Modules/Shop/Database/Migrations/2020_10_17_025153_create_shop_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->nestedSet();
+            $table->text('meta_title')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_categories');
+    }
+}
