e5d00e90dfe2c8ab600d6f0426ecf2bcd3c2c398
Seigi Takada <choji.takada@gmail.com>
2020-10-17 17:25:54 +0800

    Add shop products migration

---
+++ Modules/Shop/Database/Migrations/2020_10_17_025305_create_shop_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_products', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['BASIC', 'VARIATION', 'DOWNLOADABLE', 'VARIABLE_PRODUCT'])->default('BASIC');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sku')->nullable();
+            $table->string('barcode')->nullable();
+            $table->text('description')->nullable();
+            $table->text('meta_title')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_products');
+    }
+}
