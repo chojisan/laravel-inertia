920687103a88709ad957b12e210248883c8f65fa
Seigi Takada <choji.takada@gmail.com>
2020-10-18 16:20:18 +0800

    Register auth service provider

--- Modules/CMS/Providers/CMSServiceProvider.php
+++ Modules/CMS/Providers/CMSServiceProvider.php
@@ -38,6 +38,7 @@ class CMSServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(RouteServiceProvider::class);
+        $this->app->register(AuthServiceProvider::class);
     }
 
     /**
